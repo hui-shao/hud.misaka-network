b2d9ede2d9f2a9e59467b8790355980985854c5e
苍崎橙子 <AozakiOrenji@outlook.com>
2017-08-25 23:04:31 +0800

    new eg: hasOwnProperty

---
+++ php/hasOwnProperty.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Aozak
+ * Date: 8/25/17 025
+ * Time: 下午 11:01
+ */
+$array = Array(
+    "key1" => "value1",
+    "key2" => "value2"
+);
+echo is_null($array["key3"]);
